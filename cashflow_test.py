c731e3ace0c0a0372f912856e9a5b1332733b29c
Jeffery Williams <jeffery@jawmark.net>
2012-02-01 15:24:45 -0500

    AddIceTime test

--- cashflow_test.py
+++ cashflow_test.py
@@ -6,9 +6,13 @@ class TestCashFlowFunctions(unittest.TestCase):
       self.cf=CashFlow()
       self.x=1
 
-   def test_unit(self):
+   def test_addIce(self):
       self.cf.addIce(1000)
       self.assertEqual(self.cf.__ice__,1000)
+
+   def test_addIceTime(self):
+      self.cf.addIce(1000,time=10)
+      self.assertEqual(self.cf.__ice__,1000,time=10)
 
 if __name__ == '__main__':
    unittest.main()
