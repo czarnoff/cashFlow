199e23e5fee0be60223642b27fa7ead76d37bd2b
Jeffery Williams <jeffery@jawmark.net>
2012-02-01 15:52:24 -0500

    Adding rate to ice initializer

--- cashflow.py
+++ cashflow.py
@@ -1,8 +1,16 @@
 class CashFlow:
    __ice__=0
    __time__=0;
+   __rate__=0;
 
-   def addIce(self,iceBlock, time=0):
+   def addIce(self,iceBlock, time=0,rate=0):
       self.__ice__=iceBlock
       self.__time__=time
+      self.__rate__=rate
+      
+      if (rate != 0):
+         __time__=__ice__/__rate__
+      
+      if (time != 0):
+         __rate__=__ice__/__time__
 
