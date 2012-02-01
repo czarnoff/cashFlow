cd954c20f71012eb9a144108f52c871f30879bcf
Jeffery Williams <jeffery@jawmark.net>
2012-02-01 14:28:32 -0500

    Test of unit test suit

---
+++ cashflow_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestCashFlowFunctions(unittest.TestCase):
+   def setUp(self):
+      self.x=1
+
+   def test_unit(self):
+      self.assertEqual(x, 1)
+
+if __name__ == '__main__':
+   unittest.main()
