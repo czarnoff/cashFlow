f82e3e976f417df612bde0dee54307f0f42eab78
Jeffery Williams <jeffery@jawmark.net>
2012-02-01 15:31:46 -0500

    addIce time test and parameter fixed

--- cashflow.py
+++ cashflow.py
@@ -1,6 +1,6 @@
 class CashFlow:
    __ice__=0
-   time=0;
+   __time__=0;
 
    def addIce(self,iceBlock, time=0):
       self.__ice__=iceBlock
--- cashflow_test.py
+++ cashflow_test.py
@@ -4,7 +4,6 @@ import unittest
 class TestCashFlowFunctions(unittest.TestCase):
    def setUp(self):
       self.cf=CashFlow()
-      self.x=1
 
    def test_addIce(self):
       self.cf.addIce(1000)
@@ -12,7 +11,7 @@ class TestCashFlowFunctions(unittest.TestCase):
 
    def test_addIceTime(self):
       self.cf.addIce(1000,time=10)
-      self.assertEqual(self.cf.__ice__,1000,time=10)
+      self.assertEqual(self.cf.__time__,10)
 
 if __name__ == '__main__':
    unittest.main()
