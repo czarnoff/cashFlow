55571570e99c2ed1c0091af6dce4f0a952a7f09d
Jeffery Williams <jeffery@jawmark.net>
2012-02-01 15:46:04 -0500

    Adding rate to tests

--- cashflow_test.py
+++ cashflow_test.py
@@ -11,7 +11,15 @@ class TestCashFlowFunctions(unittest.TestCase):
 
    def test_addIceTime(self):
       self.cf.addIce(1000,time=10)
+      self.assertEqual(self.cf__ice__,1000)
       self.assertEqual(self.cf.__time__,10)
+      slef.assertEqual(self.cf.__rate__,1000/10)
+
+   def test_addIceRate(self):
+      self.cf.addIce(1000,rate=10)
+      self.assertEqual(self.cf.__ice__,1000)
+      self.assertEqual(self.cf.__rate__,10)
+      self.assertEqual(self.cf.__time__,1000/10)
 
 if __name__ == '__main__':
    unittest.main()
